eabf72628efa01e1853b51e903a4402b33e99500
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-07-11 22:19:56 +0700

    create all function to get all siswa data

---
+++ src/controller/SiswaController.ts
@@ -0,0 +1,11 @@
+import { getRepository } from "typeorm";
+import { Siswa } from "../entity/Siswa";
+import { NextFunction, Request, Response } from "express";
+
+export class SiswaController {
+  private siswaRepository = getRepository(Siswa);
+
+  async all(request: Request, response: Response, next: NextFunction) {
+    return await this.siswaRepository.find({ relations: ["kelas"] });
+  }
+}
